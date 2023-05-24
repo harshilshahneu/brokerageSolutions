a01de21a023f2a0ecdff6c7d90b95c4a4f3f950f
Harshil Shah <shah.harshil2@northeastern.edu>
2023-05-24 17:27:56 -0400

    reverting commit

---
+++ pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <!--
+// @TODO
+// @TODO
+// @TODO
+ -->
+  <div>HOME</div>
+</template>
+
+<script>
+import { auth, signInWithPhoneNumber, RecaptchaVerifier } from "../plugins/firebase";
+import loginServices from "../services/loginServices";
+export default {
+  data: () => ({}),
+  async mounted() {},
+  methods: {},
+};
+</script>
+
+<style></style>
